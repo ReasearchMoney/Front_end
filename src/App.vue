7bc62a0020dc198dc4ac8db79f9400fa53180fe3
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-09-05 19:22:51 +0900

    화면 ui 구성

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-app-bar app color="elevation-0 white" white>
+      <v-toolbar-title>리서치머니</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div class="text-right" id="nav">
+        <router-link to="/">로그인</router-link> |
+        <router-link to="/about">회원가입</router-link>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <!--body-->
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">
+* {
+  text-decoration: none;
+}
 #app {
+  background-color: white;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  // color: #2c3se50;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-carousel
+      :continuous="false"
+      :cycle="cycle"
+      :show-arrows="false"
+      hide-delimiter-background
+      delimiter-icon="mdi-minus"
+      height="300"
+    >
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet :color="colors[i]" height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="text-h2">{{ slide }} Slide</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+    <v-container class="pa-10">
+      <div class="pb-5">내가 참여하기 딱 좋은 연구만 간편하게 검색하기</div>
+
+      <v-text-field
+        class="w-8"
+        outlined
+        rounded
+        label="Search"
+        append-icon="mdi-map-marker"
+      ></v-text-field>
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      colors: [
+        "green",
+        "secondary",
+        "yellow darken-4",
+        "red lighten-2",
+        "orange darken-1",
+      ],
+      cycle: false,
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+    };
+  },
+};
 </script>
+<style scoped>
+/* .v-text-field {
+  
+} */
+</style>
