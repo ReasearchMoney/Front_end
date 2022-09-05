97f0ee862d8e2ca9dbaef5013163d1c28533c976
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-09-05 16:29:09 +0900

    first commit

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  chainWebpack: config => { config.plugin('html').tap(args => { args[0].title = 'Lajancia Portfolio'; return args; }); },
+  configureWebpack: config => {
+    
+    if (process.env.NODE_ENV === 'production') {
+        config.mode = 'production';
+                     // Package file size configuration
+        config.performance = {
+          maxEntrypointSize: 10000000,
+          maxAssetSize: 30000000
+        }
+    }
+  },
+  devServer: { 
+    proxy: { 
+      '/api': { 
+        target: 'http://localhost:3000/api',
+        changeOrigin: true, 
+        pathRewrite: { 
+          '^/api': ''
+        } 
+      } 
+    } 
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+//webpack.config.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: path.join(__dirname, 'main.js')
+  },
+  module: {
+    rules: [{
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      },
+       {
+            test: /\.(png|jpg|jpeg|gif|svg)(\?.*)?$/,
+            use: [
+                'url-loader?name=assets/[name].[ext]',
+            ]
+        }],
+    },
+   resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
+  },
+};
