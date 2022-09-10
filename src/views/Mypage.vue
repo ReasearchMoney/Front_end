97e5fa45b2f00c5fdc22f6296dcd0417366db81d
Lajancia <50996139+Lajancia@users.noreply.github.com>
2022-09-10 19:59:58 +0900

    post create

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -1,22 +1,86 @@
 <template>
   <v-container style="width: 100vh; max-width: 60%">
     <v-card color="elevation-0 background">
-      <v-card-title>
+      <v-card-title class="pt-15">
         <v-icon class="mr-2 primary--text">mdi-domain</v-icon>내가 업로드한
         연구</v-card-title
       >
-      <v-card class="mb-4 pa-2 text-sm-center">
-        <div>Oops! 아직 진행중인 연구가 없네요....:(</div>
-        <div>아래 버튼을 눌러 첫 연구를 생성해 보세요!</div>
+      <div v-if="post">
+        <v-card
+          v-for="item in post"
+          v-bind:key="item.postid"
+          class="mb-4 pa-2 text-sm-left"
+        >
+          <span class="primary--text">{{ item.state }} </span>
+          <span>{{ item.title }}</span>
+
+          <span> <v-icon>mdi-bookmark-outline</v-icon></span>
+          <div class="text--text text-caption">
+            <span>
+              <v-btn color="elevation-0 box" x-small>{{
+                item.institution
+              }}</v-btn>
+            </span>
+            <span> {{ item.start_date }}~{{ item.end_date }} |</span>
+            <span> {{ item.period }} |</span>
+            <span> {{ item.location }}</span>
+          </div>
+        </v-card>
+      </div>
+
+      <v-card v-else class="mb-4 pa-15 text-sm-center">
+        <div class="text--text">Oops! 아직 진행중인 연구가 없네요....:(</div>
+        <div class="text--text">아래 버튼을 눌러 첫 연구를 생성해 보세요!</div>
       </v-card>
-      <v-btn color="button" class="white--text" elevation="2" large
-        >+ 새 연구 생성하기</v-btn
-      >
+      <router-link to="/post">
+        <v-btn
+          style="width: 100%"
+          color="button"
+          class="white--text"
+          elevation="2"
+          large
+          >+ 새 연구 생성하기</v-btn
+        >
+      </router-link>
     </v-card>
   </v-container>
 </template>
 <style scoped>
-.v-btn {
+/* .v-btn {
   width: 100%;
-}
+} */
 </style>
+<script>
+export default {
+  data() {
+    return {
+      post: [
+        {
+          postid: 0,
+          state: "모집중",
+          title:
+            "웹 페이지 스크랩 툴의 사용 패턴 및 사용자 멘탈 모델에 관한 정성적 연구 (인터뷰)",
+          institution: "성균관대",
+          start_date: "22.09.01",
+          end_date: "22.09.23",
+          period: "1",
+          cost: "2만원",
+          location: "서울시 종로구",
+        },
+        {
+          postid: 1,
+          state: "모집중",
+          title:
+            "웹 페이지 스크랩 툴의 사용 패턴 및 사용자 멘탈 모델에 관한 정성적 연구 (인터뷰)",
+          institution: "성균관대",
+          start_date: "22.09.01",
+          end_date: "22.09.23",
+          period: "1",
+          cost: "2만원",
+          location: "서울시 종로구",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/views/Post.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container class="mb-0" style="max-width: 100%" color="white" id="top">
+    <h2>새 연구 추가</h2>
+    <v-form>
+      <v-container style="max-width: 60%">
+        <v-card class="d-flex flex-row mb-6 elevation-0">
+          <h3 class="pr-5 mt-2">제목</h3>
+
+          <v-text-field
+            v-model="myText"
+            label="Solo"
+            solo
+            accent
+            dense
+          ></v-text-field>
+        </v-card>
+        <v-card class="d-flex flex-row mb-6 elevation-0">
+          <h3 class="pr-5 mt-2">설명</h3>
+
+          <v-text-field label="Solo" solo dense></v-text-field>
+        </v-card>
+        <v-card class="d-flex flex-row mb-6 elevation-0">
+          <h3 class="pr-5 mt-2">제목</h3>
+
+          <v-text-field label="Solo" solo dense></v-text-field>
+        </v-card>
+        <v-card class="d-flex flex-row mb-6 elevation-0">
+          <h3 class="pr-5 mt-2">제목</h3>
+
+          <v-text-field
+            label="Solo"
+            solo
+            filled
+            color="accent"
+            dense
+          ></v-text-field>
+        </v-card>
+      </v-container>
+    </v-form>
+    <v-btn @click="getTest()">누르시오</v-btn>
+  </v-container>
+</template>
+<style lang="scss" scoped>
+#top {
+  background: white;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      myText: "",
+    };
+  },
+  methods: {
+    getTest() {
+      console.log(this.myText);
+    },
+  },
+};
+</script>
